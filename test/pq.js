4cdb2472d8a953a68b2615c4ae1ea50a310672d9
mikolalysenko <mikolalysenko@gmail.com>
2016-09-21 20:16:21 -0400

    priority queue now support weight decrement

--- test/pq.js
+++ test/pq.js
@@ -2,7 +2,7 @@ var tape = require('tape')
 var PriorityQueue = require('../lib/pq')
 
 tape('simple priority queue test', function (t) {
-  var pq = new PriorityQueue()
+  var pq = new PriorityQueue(2)
 
   t.equals(pq.count, 0, 'queue initially empty')
   pq.push(0, 10)
@@ -10,11 +10,11 @@ tape('simple priority queue test', function (t) {
   t.equals(pq.topItem(), 0, 'top item ok')
   t.equals(pq.topWeight(), 10, 'top weight ok')
   pq.pop()
-  t.equals(pq.count, 0)
+  t.equals(pq.count, 0, 'popping clears count')
   pq.push(1, 2)
-  t.equals(pq.count, 1)
+  t.equals(pq.count, 1, 'count now 1')
   pq.push(0, 0)
-  t.equals(pq.count, 2)
+  t.equals(pq.count, 2, 'count now 2')
 
   t.equals(pq.topItem(), 0)
   pq.pop()
@@ -29,8 +29,8 @@ tape('simple priority queue test', function (t) {
 tape('heap sort test', function (t) {
   var N = 50
   function testSort () {
-    var i
-    var pq = new PriorityQueue()
+    var i, j
+    var pq = new PriorityQueue(N)
     var items = []
     var weights = []
     var pairs = []
@@ -40,16 +40,22 @@ tape('heap sort test', function (t) {
       items.push(x)
       weights.push(w)
       pairs.push([x, w])
-      t.equals(pq.count, i)
+      t.equals(pq.count, i, 'push count ok')
       pq.push(x, w)
+      for (j = 0; j < pq.count; ++j) {
+        t.equals(pq.index[pq.items[j]], j, 'index ' + j + ' ok')
+      }
     }
-    t.equals(pq.count, N)
+    t.equals(pq.count, N, 'final count ok')
     pairs.sort(function (a, b) { return a[1] - b[1] })
 
     for (i = 0; i < N; ++i) {
       t.equals(pq.count, N - i, 'pq not empty')
       t.equals(pq.topItem(), pairs[i][0], 'top item ' + i)
       t.equals(pq.topWeight(), pairs[i][1], 'top weight ' + i)
+      for (j = 0; j < pq.count; ++j) {
+        t.equals(pq.index[pq.items[j]], j, 'index ' + j + ' ok')
+      }
       pq.pop()
     }
     t.equals(pq.count, 0, 'count cleared')
